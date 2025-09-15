743c5c6d987e87abf6d706aaaba58cee39d691f0
Raphael <raphael.girard.aytre>
2025-09-15 17:47:50 +0200

    suppression des choses compromettantes

--- Raphael/asset/js/script.js
+++ Raphael/asset/js/script.js
@@ -9,17 +9,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">marchés couverts Paris</a>'
 }).addTo(map);
 
-var customIcon = L.icon({
-    iconUrl: 'asset/photo/lagrossekatia.png',
-    iconSize: [60, 60],     // 👈 taille de l’image en pixels
-    iconAnchor: [30, 60],   // 👈 le point d’ancrage (milieu bas de l’image)
-    popupAnchor: [0, -60]   // 👈 le popup sort au-dessus de l’image
-});
-
 datas.forEach(data => {
 
     //placement des curseurs aux données de géolocalisation des marchés
-    var marker = L.marker([data.geo_point_2d.lat, data.geo_point_2d.lon], { icon: customIcon }).addTo(map)
+    var marker = L.marker([data.geo_point_2d.lat, data.geo_point_2d.lon]).addTo(map)
 
     //ajout d'infos complémentaires des marchés
     marker.bindPopup("<b>" + data.nom_court + "</b> <br> Les jours de marché sont : " + data.jours_tenue).openPopup();
