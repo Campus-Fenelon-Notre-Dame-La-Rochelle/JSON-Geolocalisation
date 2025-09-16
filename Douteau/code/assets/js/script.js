3f4d85a536f91c1afef106314951af09075ed38a
Terry <terry.douteau17@gmail.com>
2025-09-16 15:55:06 +0200

    terry

--- Douteau/code/assets/js/script.js
+++ Douteau/code/assets/js/script.js
@@ -11,20 +11,29 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(map);
 
 //Parcours du tableau et affichage des points sur la carte
+var placeHandicapes = L.layerGroup([]);
 datasHandicapes.forEach(
         (data)=>{
                 //Marker
                 var marker = L.marker([data.fields.geo_point_2d[1], data.fields.geo_point_2d[0]]).addTo(map);
-                
                 //Popup 
-                marker.bindPopup(`<b>Adresse : ${data.fields.adresse}.</b><br/><b>Observation : ${data.fields.obs}</b>`)
+                marker.bindPopup(`<b>Adresse : ${data.fields.adresse}.</b><br/><b>Observation : ${data.fields.obs}</b>`);
+                placeHandicapes.addLayer(marker);
         }
 );
 
+var wc = L.layerGroup([]);
 datasWC.forEach(
         (data)=>{
                 var marker = L.marker([data.fields.geojson.coordinates[1], data.fields.geojson[0]]).addTo(map);
-
                 marker.bindPopup(`<b>Emplacement : ${data.fields.emplacement}.</b><br/><b>Horaires : ${data.fields.horaires}.</b><br/><b>Surveillance : ${data.fields.surveillance}.</b>`);
+                wc.addLayer(marker);
         }
 );
+
+var overlayMaps = {
+        "Parking handicapés": placeHandicapes,
+        "Toilettes public" : wc
+    };
+
+var layerControl = L.control.layers(null, overlayMaps).addTo(map);
