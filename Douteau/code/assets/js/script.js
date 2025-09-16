330e765b35e67172c67e7a51706aad15296300ca
Terry <terry.douteau17@gmail.com>
2025-09-16 16:25:35 +0200

    terry

--- Douteau/code/assets/js/script.js
+++ Douteau/code/assets/js/script.js
@@ -14,11 +14,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //Parcours du tableau et affichage des points sur la carte
 var placeHandicapes = L.layerGroup([]);
 datasHandicapes.forEach(
-        (data)=>{
+        (parking)=>{
                 //Marker
-                var marker = L.marker([data.fields.geo_point_2d[1], data.fields.geo_point_2d[0]]).addTo(map);
+                var marker = L.marker([parking.fields.geo_point_2d[1], parking.fields.geo_point_2d[0]]).addTo(map);
                 //Popup 
-                marker.bindPopup(`<b>Adresse : ${data.fields.adresse}.</b><br/><b>Observation : ${data.fields.obs}</b>`);
+                marker.bindPopup(`<b>Adresse : ${parking.fields.adresse}.</b><br/><b>Observation : ${parking.fields.obs}</b>`);
                 placeHandicapes.addLayer(marker);
         }
 );
@@ -26,7 +26,7 @@ datasHandicapes.forEach(
 var wc = L.layerGroup([]);
 datasWC.forEach(
         (data)=>{
-                var marker = L.marker([data.fields.geojson.coordinates[1], data.fields.geojson[0]]).addTo(map);
+                var marker = L.marker([data.fields.geo_point_2d[1], data.fields.geo_point_2d[0]]).addTo(map);
                 marker.bindPopup(`<b>Emplacement : ${data.fields.emplacement}.</b><br/><b>Horaires : ${data.fields.horaires}.</b><br/><b>Surveillance : ${data.fields.surveillance}.</b>`);
                 wc.addLayer(marker);
         }
@@ -34,7 +34,7 @@ datasWC.forEach(
 
 var overlayMaps = {
         "Parking handicapés": placeHandicapes,
-        "Toilettes public" : wc
+        "Toilettes public": wc
     };
 
 var layerControl = L.control.layers(null, overlayMaps).addTo(map);
